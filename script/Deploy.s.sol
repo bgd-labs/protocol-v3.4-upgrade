e090c03c51b8017684a30d45bbb509286850ff3d
TepNik <tepelin.nikita@gmail.com>
2025-03-21 12:50:45 +0100

    Deleted forge console import from deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import "forge-std/console.sol";
-
 import {
   EthereumScript,
   PolygonScript,
@@ -296,8 +294,6 @@ library DeploymentLibrary {
       type(ATokenWithDelegationInstance).creationCode,
       abi.encode(AaveV3Ethereum.POOL, AaveV3Ethereum.DEFAULT_INCENTIVES_CONTROLLER, AaveV3Ethereum.COLLECTOR)
     );
-
-    console.log("Deploying UpgradePayloadMainnet...");
 
     return GovV3Helpers.deployDeterministic(
       type(UpgradePayloadMainnet).creationCode,
