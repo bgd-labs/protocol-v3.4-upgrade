35ccaa66e87c8357f1a3d660a5db60c170108d19
Lukas <lukasstrassel@googlemail.com>
2025-05-07 13:24:58 +0200

    fix: rename vars

--- test/Mainnet.t.sol
+++ test/Mainnet.t.sol
@@ -16,7 +16,7 @@ import {VariableDebtTokenMainnetInstanceGHO} from "../src/VariableDebtTokenMainn
 
 import {UpgradeTest, IERC20} from "./UpgradeTest.t.sol";
 
-contract MainnetTest is UpgradeTest("mainnet", 22331905) {
+contract MainnetTest is UpgradeTest("mainnet", 22431434) {
   using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
   using WadRayMath for uint256;
 
