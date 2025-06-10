5b090c765a63e58dba0af2fe6885358ec7ee58eb
Lukas <lukasstrassel@googlemail.com>
2025-06-10 12:12:41 +0200

    feat: add gnosis

--- src/Deployments.sol
+++ src/Deployments.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.10;
 
 library Deployments {
   address public constant BASE = 0x41d7188B68A6b334071DB5A1D33Ce4b9109899B3;
+  address public constant GNOSIS = 0x6f6A25C4D03EdA3800863522d1340954021046F5;
 }
